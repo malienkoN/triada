bf2f8dff758ff050a5b0dd563ce68bc50331da09
Nikita <nekmalienko2001@gmail.com>
2021-03-17 21:34:01 +0200

    product 100%

--- source/js/app.js
+++ source/js/app.js
@@ -177,6 +177,21 @@ $(document).ready(function () {
             }
         }
     });
+
+    /* tabs */
+    $('.tabs__triggers-item').click(function (e) {
+        e.preventDefault();
+
+        $('.tabs__triggers-item').removeClass('tabs__triggers-item--active');
+        $('.tabs__content-item').removeClass('tabs__content-item--active');
+
+        $(this).addClass('tabs__triggers-item--active');
+        $($(this).attr('href')).addClass('tabs__content-item--active');
+    });
+
+    $('.tabs__triggers-item:first').click();
+
+    /* /tabs */
 });
 
 /* Smooth scroll to the anchors */
